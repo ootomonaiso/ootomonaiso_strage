08c9da55f0d46a459df4e9ae824b525ad34db84e
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2025-03-18 19:54:39 +0900

    不要なファイルと設定を削除

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["imagedelivery.net"], // Cloudflare Images
-  },
-};
-
-export default nextConfig;
