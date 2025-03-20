287efb70da15ae8593180cc783a3560315a4c915
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2025-03-20 22:14:25 +0900

    drizzle.config.tsの設定を更新し、スキーマパスと出力先を変更、ダイアレクトをPostgreSQLに設定

--- pro/drizzle.config.ts
+++ pro/drizzle.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  driver: "supabase",
-  dbCredentials: {
-    accountId: process.env.SUPABASE_ACCOUNT_ID!,
-    databaseId: process.env.SUPABASE_DATABASE_ID!,
-    token: process.env.SUPABASE_TOKEN!,
-  },
+  schema: "./src/schema.ts",
+  out: "./supabase/migrations",
+  dialect: "postgresql",
 });
