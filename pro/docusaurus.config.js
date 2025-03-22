8dc312fba6e51c638b5e0f60dd7ea922d0d025fa
ootomonaiso <ootomonaiso.tubusiki@gmail.com>
2025-03-23 01:39:08 +0900

    fix: favicon.icoとdocusaurus.config.jsの修正
    - favicon.icoを更新
    - docusaurus.config.jsのタイトルを'My Site'から'ootomonaiso blog'に変更し、ロゴのaltテキストを'logo'に修正
    - deploy.ymlのキャッシュ機能のコメントを削除

--- pro/docusaurus.config.js
+++ pro/docusaurus.config.js
@@ -55,9 +55,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'ootomonaiso blog',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'logo',
           src: 'img/logo.svg',
         },
         items: [
